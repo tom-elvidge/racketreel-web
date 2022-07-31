91a87eb097a90efe5cfc76fe6cf591e6abbc8fcf
tom-elvidge <tomelvidge1999@gmail.com>
2022-07-31 12:36:36 +0100

    bootstrapped svelte

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Router from './routes/index.svelte';
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+</style>
+
+<main>
+  <Router />
+</main>
